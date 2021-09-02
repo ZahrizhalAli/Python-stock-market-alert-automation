b95f3a505f09f85631859e947bf657d279f9fb99
zahrizhalali29 <zzahrizhal.ali@gmail.com>
2021-09-02 15:31:30 +0800

    Update baru

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import datetime as dt
 from decouple import config
+from twilio.rest import Client
+
+account_sid = config('ACCOUNT_SID')
+auth_token = config('AUTH_TOKEN')
+client = Client(account_sid, auth_token)
 
 time = dt.datetime.now(dt.timezone.utc)
 time_now = str(time)
@@ -31,45 +36,40 @@ data = response.json()['Time Series (Daily)']
 data_list = [value for (key, value) in data.items()]  # List Comprehension
 yesterday_data = data_list[0]
 yesterday_closing_data = yesterday_data['4. close']
-print(yesterday_closing_data)
+# print(yesterday_closing_data)
 
 before_yesterday = data_list[1]
 before_yesterday_closing_data = before_yesterday['4. close']
-print(before_yesterday_closing_data)
+# print(before_yesterday_closing_data)
 
 difference_price = abs(float(yesterday_closing_data) - float(before_yesterday_closing_data))
-print(difference_price)
+# print(difference_price)
+up_down = None
+if difference_price > 0:
+    up_down = "⬆"
+else:
+    up_down = "⬇"
+
 
 diff_percent = (difference_price / float(yesterday_closing_data)) * 100
-print(f"{diff_percent}%")
+# print(f"{diff_percent}%")
 
-if diff_percent > 0:
+if diff_percent >= 5:
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()['articles']
     three_articles = articles[:3]
-    print(three_articles)
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    # print(three_articles)
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{round(diff_percent)}% \nHeadline: {article['title']}. "
+                          f"\n Brief: {article['description']}" for article in three_articles]
+    for ar in formatted_articles:
+        message = client.messages \
+            .create(
+            body=ar,
+            from_='YOUR_PHONE_NUMBER_API_SERVICE',
+            to='YOUR_PHONE_NUMBER'
+        )
 
 
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+
 
